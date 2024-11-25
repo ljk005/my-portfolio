cec84eb7887ece15bae58b8c2cf9e72255049d84
ljk <logan.j.knies@gmail.com>
2024-11-25 11:43:53 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <Container>
         <Title order={1}>Welcome to My Portfolio</Title>
         <Text mt="sm">
-          Hi, I'm Logan! I love exploring, learning, and meeting new people. Here are some of my projects!
+          Hi, I'm Logan! I love exploring, learning, and meeting new people. Here are some of my projects
         </Text>
         <Group mt="lg">
           <Button component="a" href="#projects" variant="outline">
