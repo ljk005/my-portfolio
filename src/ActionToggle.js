fb094537698f43c2614d4d7e1e0fb9359d4d2f01
ljk <logan.j.knies@gmail.com>
2024-11-25 12:04:29 -0600

    New ActionToggle, I thought I just committed this

---
+++ src/ActionToggle.js
@@ -0,0 +1,23 @@
+import { IconMoon, IconSun } from '@tabler/icons-react';
+import cx from 'clsx';
+import { ActionIcon, Group, useComputedColorScheme, useMantineColorScheme } from '@mantine/core';
+import './ActionToggle.css';  // Assuming you're creating the CSS file in the same directory.
+
+export function ActionToggle() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+
+  return (
+    <Group justify="center">
+      <ActionIcon
+        onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+        variant="default"
+        size="xl"
+        aria-label="Toggle color scheme"
+      >
+        <IconSun className={cx('icon', 'light')} stroke={1.5} />
+        <IconMoon className={cx('icon', 'dark')} stroke={1.5} />
+      </ActionIcon>
+    </Group>
+  );
+}
